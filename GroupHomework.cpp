5d50ddc33f0d842d387aa175e70192fa59af25fa
ErylLadrido <eryl_ladrido@dlsu.edu.ph>
2025-05-13 13:19:36 +0800

    Initial Codebase
    Initial Menu

---
+++ GroupHomework.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <cstdlib> // For system("clear") or system("cls")
+
+using namespace std;
+
+// Function to display the CSOPESY ASCII header
+void displayHeader() {
+    cout << R"(
+   _____         ____. ___________ .____       ________      _________
+  /  _  \       |    | \_   _____/ |    |      \_____  \    /   _____/
+ /  /_\  \      |    |  |    __)_  |    |       /   |   \   \_____  \ 
+/    |    \ /\__|    |  |        \ |    |___   /    |    \  /        \
+\____|____/ \________| /_________/ |________\  \_________/ /_________/
+)" << endl;
+}
+
+int main() {
+    string command;
+    
+    // Display initial messages
+    displayHeader();
+    cout << "Hello, Welcome to AJEL OS command.net" << endl;
+    cout << "Type \"exit\" to quit, \"clear\" to clear the screen" << endl;
+    
+    // Main command loop
+    //while (true) {
+        cout << "\nEnter a command: ";
+
+    //}
+    
+    return 0;
+}
