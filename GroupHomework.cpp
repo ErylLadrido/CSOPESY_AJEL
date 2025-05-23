2927b1e34ebe5a80e232e8669ed2699214ceb106
44shleyy <115363977+44shleyy@users.noreply.github.com>
2025-05-23 19:37:24 +0800

    Update GroupHomework.cpp

--- GroupHomework.cpp
+++ GroupHomework.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 #include <string>
 #include <cstdlib> // For system("clear") or system("cls")
+#include <map>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 
 using namespace std;
+
+class Screen {
+public:
+    string name;
+    int currentLine;
+    int totalLines;
+    string timestamp;
+
+    Screen(string n) {
+        name = n;
+        currentLine = 1;
+        totalLines = 100; // placeholder
+        timestamp = getCurrentTimestamp();
+    }
+
+    void display() {
+        cout << "\n=== Screen: " << name << " ===\n";
+        cout << "Process name: " << name << endl;
+        cout << "Instruction line: " << currentLine << " / " << totalLines << endl;
+        cout << "Created at: " << timestamp << endl;
+        cout << "Type 'exit' to return to main menu.\n";
+    }
+
+private:
+    string getCurrentTimestamp() {
+        time_t now = time(nullptr);
+        tm* localTime = localtime(&now);
+        stringstream ss;
+        ss << put_time(localTime, "%m/%d/%Y, %I:%M:%S %p");
+        return ss.str();
+    }
+};
+
 
 // Function to display the CSOPESY ASCII header
 void displayHeader() {
@@ -22,8 +59,14 @@ void showCommands() {
     cout << "  exit   - Exit the program\n";
 }
 
+
+
 int main() {
     string command;
+    map<string, Screen> screens;
+    bool inScreen = false;
+    string currentScreen = "";
+
 
     // Display initial messages
     displayHeader();
@@ -35,6 +78,19 @@ int main() {
         cout << "\nEnter a command: ";
         getline(cin, command);
 
+        if (inScreen) {
+            if (command == "exit") {
+                inScreen = false;
+                 currentScreen = "";
+                 displayHeader();
+                cout << "Back to main menu." << endl;
+            } else {
+                cout << "[" << currentScreen << "] " << command << " command received.\n";
+                screens[currentScreen].currentLine++;
+             }
+            continue;
+        }   
+
         if (command == "exit") {
             cout << "exit command recognized. Exiting application." << endl;
             break; // Exit the loop and end the program
@@ -44,6 +100,25 @@ int main() {
             displayHeader();
             cout << "Hello, Welcome to AJEL OS command.net" << endl;
             cout << "Type \"exit\" to quit, \"clear\" to clear the screen" << endl;
+        }
+        else if (command.substr(0, 9) == "screen -r") {
+            string name = command.substr(10);
+            if (screens.find(name) != screens.end()) {
+                inScreen = true;
+                currentScreen = name;
+                screens[name].display();
+            } else {
+                cout << "No screen found with name '" << name << "'." << endl;
+            }
+        }
+        else if (command.substr(0, 9) == "screen -s") {
+            string name = command.substr(10);
+            if (screens.find(name) == screens.end()) {
+                screens[name] = Screen(name);
+                cout << "New screen '" << name << "' created." << endl;
+            } else {
+                cout << "Screen '" << name << "' already exists." << endl;
+            }
         }
         else {
             cout << command << " command recognized. Doing something." << endl;
